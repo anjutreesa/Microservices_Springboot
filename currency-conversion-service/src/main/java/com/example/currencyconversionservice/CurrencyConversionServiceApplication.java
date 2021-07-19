0cc7bcd0c37d33c01375c7b6e94faee588f7e547
anjutreesa <84172498+anjutreesa@users.noreply.github.com>
2021-07-19 17:10:29 +0530

    CircuitBreaker

--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,11 +2,8 @@ package com.example.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
-import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -14,11 +11,6 @@ public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
-	}
-
-	@Bean
-	public Sampler defaultSampler() {
-		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.example.currencyconversionservice.bean.CurrencyConversionBean;
 
-@FeignClient(name="netflix-zuul-api-gateway-server")
-@RibbonClient(name="currency-exchange-service")
+@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeServiceProxy {
 
 	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
